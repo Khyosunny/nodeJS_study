58c03e2254e4f7c2b9b0cf02d43d8df8d296dfb8
Khyosunny <kimhyo_0218@naver.com>
2021-01-05 21:44:12 +0900

    react router 작업

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
