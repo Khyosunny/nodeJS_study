5dd5b9e3ff998ab8e9c4928cb3dd265b4c32b2cc
Khyosunny <kimhyo_0218@naver.com>
2021-01-06 21:52:33 +0900

    로그인 기능

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,12 +3,22 @@ import axios from 'axios'
 
 function LandingPage() {
   useEffect(() => {
-    axios
-      .get('http://localhost:5000/api/hello')
-      .then(response => console.log(response.data))
+    axios.get('/api/hello').then(response => console.log(response))
   }, [])
 
-  return <div>LandingPage</div>
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <h2>시작페이지</h2>
+    </div>
+  )
 }
 
 export default LandingPage
